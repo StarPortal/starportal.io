f4fb7a1f285d276cd092c48db2a13cdffbae61d0
蒼時弦也 <elct9620@frost.tw>
2022-05-25 19:26:57 +0800

    Update Hero section

--- src/partials/Features.jsx
+++ src/partials/Features.jsx
@@ -22,7 +22,7 @@ function Features() {
   }, [tab])
 
   return (
-    <section className="relative">
+    <section id="features" className="relative">
 
       {/* Section background (needs .relative class on parent and next sibling elements) */}
       <div className="absolute inset-0 bg-gray-100 pointer-events-none mb-16" aria-hidden="true"></div>
